4bc7a594ef1142281d0cb3821473f7b264cafba4
aabbcczz <aabbcczz@live.com>
2021-11-25 01:27:32 +0800

    measure the latency of DGI capturing and DX rendering

--- Desktop/DirectWrite/SimpleHelloWorld/Program.cs
+++ Desktop/DirectWrite/SimpleHelloWorld/Program.cs
@@ -20,11 +20,20 @@
 
 using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Drawing;
 using SharpDX;
 using SharpDX.Direct2D1;
 using SharpDX.DirectWrite;
 using SharpDX.Samples;
 using TextAntialiasMode = SharpDX.Direct2D1.TextAntialiasMode;
+using Vanara.PInvoke;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 
 namespace SimpleHelloWorld
 {
@@ -36,8 +45,28 @@ namespace SimpleHelloWorld
     /// </summary>
     public class Program :  Direct2D1WinFormDemoApp
     {
+        enum DrawState { Draw, ReDraw, DrawLatency };
+
         public TextFormat TextFormat { get; private set; }
-        public RectangleF ClientRectangle { get; private set; }
+        public SharpDX.RectangleF ClientRectangle { get; private set; }
+
+        private readonly ScreenStateLogger _ssl = new ScreenStateLogger();
+        private readonly List<(long timestamp, Image<Bgr, byte> image)> _renderedImages = new List<(long timestamp, Image<Bgr, byte> image)>();
+        private readonly List<(long timestamp, Image<Bgr, byte> image)> _capturedImages = new List<(long timestamp, Image<Bgr, byte> image)>();
+
+        private DrawState _drawState = DrawState.Draw;
+
+        private bool _renderStarted = false;
+        private bool _renderStopped = false;
+
+        private int _drawCount = 0;
+        private int _redrawCount = 0;
+        private int _recaptureCount = 0;
+
+        private double _latencyInMillisecond = 0.0;
+
+        private Task _captureTask;
+
         protected override void Initialize(DemoConfiguration demoConfiguration)
         {
             base.Initialize(demoConfiguration);
@@ -47,20 +76,221 @@ namespace SimpleHelloWorld
 
             RenderTarget2D.TextAntialiasMode = TextAntialiasMode.Cleartype;
 
-            ClientRectangle = new RectangleF(0, 0, demoConfiguration.Width, demoConfiguration.Height);
+            ClientRectangle = new SharpDX.RectangleF(0, 0, demoConfiguration.Width, demoConfiguration.Height);
 
-            SceneColorBrush.Color = Color.Black;               
+            SceneColorBrush.Color = SharpDX.Color.Black;
+
+            HWND handle = (HWND)DisplayHandle;
+
+            var clientRect = SystemHelper.GetClientRect(handle);
+
+            var screenRect = SystemHelper.ClientToScreen(handle, clientRect);
+
+            System.Drawing.Rectangle[] screenAreas = new System.Drawing.Rectangle[]
+            {
+                screenRect,
+            };
+
+            _captureTask = Task.Run(() => { CaptureTask(screenAreas); });
+        }
+
+        private void CaptureTask(System.Drawing.Rectangle[] screenAreas)
+        {
+            this._ssl.Run(
+                screenAreas, 
+                (System.Drawing.Bitmap[] capturedPictures, long timestamp) =>
+                {
+                    if (_renderStarted)
+                    {
+                        var image = capturedPictures[0];
+
+                        var clonedImage = image.ToImage<Bgr, byte>();
+                        _capturedImages.Add((timestamp, clonedImage));
+                    }
+
+                    return !_renderStopped;
+                });
         }
 
         protected override void Draw(DemoTime time)
         {
             base.Draw(time);
 
-            RenderTarget2D.Clear(Color.White);
+            string text = string.Empty;
 
-            //string text = "Hello World using DirectWrite!";
-            string text = Stopwatch.GetTimestamp().ToString();
-            RenderTarget2D.DrawText(text, TextFormat, ClientRectangle, SceneColorBrush);
+            if (_drawState == DrawState.Draw)
+            {
+                RenderTarget2D.Clear(SharpDX.Color.White);
+
+                if (_drawCount >= 80)
+                {
+                    if (!_renderStopped)
+                    {
+                        _renderStopped = true;
+
+                        _captureTask.Wait();
+
+                        _drawState = DrawState.ReDraw;
+                    }
+                }
+                else
+                {
+                    //string text = "Hello World using DirectWrite!";
+                    var timestamp = Stopwatch.GetTimestamp();
+
+                    text = timestamp.ToString();
+
+                    RenderTarget2D.DrawText(text, TextFormat, ClientRectangle, SceneColorBrush);
+
+                    if (_drawCount > 40) // warm up
+                    {
+                        if (!_renderStarted)
+                        {
+                            _renderStarted = true;
+                        }
+
+                        _renderedImages.Add((timestamp, null));
+                    }
+
+                    ++_drawCount;
+                }
+            }
+            else if (_drawState == DrawState.ReDraw)
+            {
+                if (_recaptureCount < _redrawCount)
+                {
+                    var image = SystemHelper.CaptureScreen((HWND)DisplayHandle, SystemHelper.GetClientRect((HWND)DisplayHandle));
+                    _renderedImages[_recaptureCount] = (_renderedImages[_recaptureCount].timestamp, image.ToImage<Bgr, byte>());
+
+                    ++_recaptureCount;
+                }
+                else
+                {
+                    if (_redrawCount >= _renderedImages.Count)
+                    {
+#if DEBUG
+                        //SaveImages();
+#endif
+                        UpdateLatency();
+                        _drawState = DrawState.DrawLatency;
+                    }
+                    else
+                    {
+                        RenderTarget2D.Clear(SharpDX.Color.White);
+
+                        text = _renderedImages[_redrawCount].timestamp.ToString();
+
+                        RenderTarget2D.DrawText(text, TextFormat, ClientRectangle, SceneColorBrush);
+
+                        ++_redrawCount;
+                    }
+                }
+            }
+            else if (_drawState == DrawState.DrawLatency)
+            {
+                RenderTarget2D.Clear(SharpDX.Color.White);
+
+                text = $"{_latencyInMillisecond:0.0000} ms";
+
+                RenderTarget2D.Clear(SharpDX.Color.White);
+                RenderTarget2D.DrawText(text, TextFormat, ClientRectangle, SceneColorBrush);
+            }
+        }
+
+        private void SaveImages()
+        {
+            foreach (var (timestamp, image) in _renderedImages)
+            {
+                image.Save($"{timestamp}.render.bmp");
+            }
+
+            foreach (var (timestamp, image) in _capturedImages)
+            {
+                image.Save($"{timestamp}.capture.bmp");
+            }
+        }
+
+        private void UpdateLatency()
+        {
+            int maxUsableDataCount = Math.Min(_renderedImages.Count, _capturedImages.Count);
+
+            double totalLatency = 0.0;
+            int totalCount = 0;
+
+            int lastMatchIndex = -1;
+
+            for (int i = 0; i < maxUsableDataCount; ++i)
+            {
+                int startMatchIndex = lastMatchIndex + 1;
+                while (startMatchIndex < _capturedImages.Count)
+                {
+                    if (_capturedImages[startMatchIndex].timestamp <= _renderedImages[i].timestamp)
+                    {
+                        ++startMatchIndex;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (startMatchIndex >= _capturedImages.Count)
+                {
+                    break;
+                }
+
+                var matchScores = Enumerable.Range(startMatchIndex, Math.Min(_capturedImages.Count - startMatchIndex, 10))
+                    .Select(idx => MatchTwoImages(_renderedImages[i].image, _capturedImages[idx].image))
+                    .ToList();
+
+                var maxMatchScore = matchScores.Max();
+                if (maxMatchScore < 0.9) 
+                {
+                    // ignore it
+                    continue;
+                }
+
+                int maxMatchIndex = matchScores.FindIndex(v => v == maxMatchScore);
+
+                int actualMatchIndex = maxMatchIndex + startMatchIndex;
+
+                var latency = (double)(_capturedImages[actualMatchIndex].timestamp - _renderedImages[i].timestamp) * 1000.0 / Stopwatch.Frequency;
+                totalLatency += latency;
+                totalCount++;
+
+                lastMatchIndex = actualMatchIndex;
+            }
+
+            if (totalCount > 0)
+            {
+                _latencyInMillisecond = totalLatency / totalCount;
+            }
+            else
+            {
+                _latencyInMillisecond = 0.0;
+            }
+        }
+
+        private static double MatchTwoImages(Image<Bgr, byte> image1, Image<Bgr, byte> image2)
+        {
+            if (image1.Width < image2.Width && image1.Height < image2.Height)
+            {
+                var temp = image1;
+                image1 = image2;
+                image2 = temp;
+            }
+
+            if (image1.Width < image2.Width || image1.Height < image2.Height)
+            {
+                throw new ArgumentException("image size is not proper for matching");
+            }
+
+            using (var result = image1.MatchTemplate(image2, TemplateMatchingType.CcoeffNormed))
+            {
+                result.MinMax(out double[] minValues, out double[] maxValues, out var minValueLocs, out var maxValueLocs);
+
+                return maxValues[0];
+            }
         }
 
         /// <summary>
---
+++ Desktop/DirectWrite/SimpleHelloWorld/ScreenStateLogger.cs
@@ -0,0 +1,300 @@
+﻿using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Vanara.PInvoke;
+
+// copy from https://github.com/sharpdx/SharpDX-Samples/tree/master/Desktop/Direct3D11.1/ScreenCapture
+// and also refer to https://github.com/google/latency-benchmark/blob/master/src/win/screenscraper.cpp
+namespace SimpleHelloWorld
+{
+    internal sealed class ScreenStateLogger
+    {
+        public ScreenStateLogger()
+        {
+        }
+
+        public void Run(Func<Bitmap[], long, bool> screenRefreshFunction)
+        {
+            var rects = new System.Drawing.Rectangle[] { new System.Drawing.Rectangle(0, 0, 0, 0) };
+
+            Run(rects, screenRefreshFunction);
+        }
+
+        public bool Run(HWND hwnd, System.Drawing.Rectangle[] clientAreas, Func<Bitmap[], long, bool> screenRefreshFunction)
+        {
+            var rects = clientAreas.Select(clientArea => SystemHelper.ClientToScreen(hwnd, clientArea)).ToArray();
+
+            if (rects.Any(r => r.Left < 0 || r.Right < 0 || r.Top < 0 || r.Bottom < 0))
+            {
+                return false;
+            }
+
+            Run(rects, screenRefreshFunction);
+
+            return true;
+        }
+
+        public void Run(System.Drawing.Rectangle[] screenAreas, Func<Bitmap[], long, bool> screenRefreshFunction)
+        {
+            if (screenRefreshFunction == null)
+            {
+                throw new ArgumentNullException(nameof(screenRefreshFunction));
+            }
+
+            Factory1 factory = null;
+            Adapter1 adapter = null;
+            SharpDX.Direct3D11.Device device = null;
+            Output output = null;
+            Output1 output1 = null;
+
+            try
+            {
+                factory = new Factory1();
+                //Get first adapter
+                adapter = factory.GetAdapter1(0);
+                //Get device from adapter
+                device = new SharpDX.Direct3D11.Device(adapter);
+                //Get front buffer of the adapter
+                output = adapter.GetOutput(0);
+                output1 = output.QueryInterface<Output1>();
+
+                // double check input screen capture area data and correct it if possible
+                screenAreas = screenAreas.Select(
+                    area =>
+                    {
+                        // Width/Height of desktop to capture
+                        int desktopWidth = output.Description.DesktopBounds.Right;
+                        int desktopHeight = output.Description.DesktopBounds.Bottom;
+
+                        int x = area.Left;
+                        int y = area.Top;
+                        int width = Math.Min(area.Width, desktopWidth - x);
+                        int height = Math.Min(area.Height, desktopHeight - y);
+
+                        if (x < 0 || y < 0 || width <= 0 || height <= 0)
+                        {
+                            throw new ArgumentOutOfRangeException("invalid screen capture rectangle");
+                        }
+
+                        return new System.Drawing.Rectangle(x, y, width, height);
+                    }).ToArray();
+
+                // function stop flag
+                bool stopped = false;
+
+                // Duplicate the output
+                using (var duplicatedOutput = output1.DuplicateOutput(device))
+                {
+                    var workItemQueue
+                        = new ConcurrentQueue<(List<(Texture2D texture, DataBox mapSource)> resources, long timestamp)>();
+
+                    var finishedWorkItemQueue
+                        = new ConcurrentQueue<(List<(Texture2D texture, DataBox mapSource)> resources, long timestamp)>();
+
+                    var callbackTask = Task.Run(() =>
+                    {
+                        while (!stopped)
+                        {
+                            if (!workItemQueue.TryDequeue(out var workItem))
+                            {
+                                continue;
+                            }
+
+                            var bitmaps = new Bitmap[workItem.resources.Count];
+                            for (int i = 0; i < workItem.resources.Count; ++i)
+                            {
+                                var (texture, mapSource) = workItem.resources[i];
+
+                                var bitmap = new Bitmap(screenAreas[i].Width, screenAreas[i].Height, PixelFormat.Format32bppArgb);
+
+                                // Copy pixels from screen capture Texture to GDI bitmap
+                                var rect = new System.Drawing.Rectangle(0, 0, screenAreas[i].Width, screenAreas[i].Height);
+
+                                var mapDest = bitmap.LockBits(rect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
+                                var sourcePtr = IntPtr.Add(mapSource.DataPointer, 0);
+                                var destPtr = mapDest.Scan0;
+
+                                for (int y = 0; y < rect.Height; y++)
+                                {
+                                    // Copy a single line 
+                                    Utilities.CopyMemory(destPtr, sourcePtr, rect.Width * 4);
+
+                                    // Advance pointers
+                                    sourcePtr = IntPtr.Add(sourcePtr, mapSource.RowPitch);
+                                    destPtr = IntPtr.Add(destPtr, mapDest.Stride);
+                                }
+
+                                // Release source and dest locks
+                                bitmap.UnlockBits(mapDest);
+
+                                bitmaps[i] = bitmap;
+                            }
+
+                            finishedWorkItemQueue.Enqueue(workItem);
+
+                            // callback
+                            if (!screenRefreshFunction(bitmaps, workItem.timestamp))
+                            {
+                                stopped = true;
+                            }
+
+                            // release bitmaps
+                            foreach (var bitmap in bitmaps)
+                            {
+                                if (bitmap != null)
+                                {
+                                    bitmap.Dispose();
+                                }
+                            }
+                        }
+
+                        // clean queued work items
+                        while (workItemQueue.TryDequeue(out var workItem))
+                        {
+                            finishedWorkItemQueue.Enqueue(workItem);
+                        }
+                    });
+
+                    while (!stopped)
+                    {
+                        try
+                        {
+                            // Try to get duplicated frame within given time is ms
+                            duplicatedOutput.AcquireNextFrame(Timeout.Infinite, out var duplicateFrameInformation, out var screenResource);
+
+                            if (duplicateFrameInformation.AccumulatedFrames == 0) // actual desktop image not changed
+                            {
+                                screenResource.Dispose();
+                                duplicatedOutput.ReleaseFrame();
+                                continue;
+                            }
+
+                            // get timestamp
+                            var frameAvailableTimestamp = System.Diagnostics.Stopwatch.GetTimestamp();
+
+                            // get data for each area
+                            var associatedDXResources = new List<(Texture2D texture, DataBox mapSource)>(screenAreas.Length);
+                            foreach (var area in screenAreas)
+                            {
+                                // Create Staging texture CPU-accessible
+                                var textureDesc = new Texture2DDescription
+                                {
+                                    CpuAccessFlags = CpuAccessFlags.Read,
+                                    BindFlags = BindFlags.None,
+                                    Format = Format.B8G8R8A8_UNorm,
+                                    Width = area.Width,
+                                    Height = area.Height,
+                                    OptionFlags = ResourceOptionFlags.None,
+                                    MipLevels = 1,
+                                    ArraySize = 1,
+                                    SampleDescription = { Count = 1, Quality = 0 },
+                                    Usage = ResourceUsage.Staging
+                                };
+
+                                var screenTexture = new Texture2D(device, textureDesc);
+                                // copy resource into memory that can be accessed by the CPU
+                                using (var screenTexture2D = screenResource.QueryInterface<Texture2D>())
+                                {
+                                    var region = new ResourceRegion()
+                                    {
+                                        Left = area.X,
+                                        Top = area.Top,
+                                        Right = area.Right,
+                                        Bottom = area.Bottom,
+                                        Front = 0,
+                                        Back = 1,
+                                    };
+
+                                    device.ImmediateContext.CopySubresourceRegion(screenTexture2D, 0, region, screenTexture, 0);
+                                }
+
+                                // Get the desktop capture texture
+                                var mapSource = device.ImmediateContext.MapSubresource(screenTexture, 0, MapMode.Read, SharpDX.Direct3D11.MapFlags.None);
+
+                                associatedDXResources.Add((screenTexture, mapSource));
+                            }
+
+                            workItemQueue.Enqueue((associatedDXResources, frameAvailableTimestamp));
+
+                            // release screen resource
+                            screenResource.Dispose();
+
+                            // release frame
+                            duplicatedOutput.ReleaseFrame();
+
+                            // release at most 3 finished work items to avoid block next frame.
+                            for (int i = 0; i < 3; ++i)
+                            {
+                                if (finishedWorkItemQueue.TryDequeue(out var finishedWorkItem))
+                                {
+                                    foreach (var (texture, mapSource) in finishedWorkItem.resources)
+                                    {
+                                        device.ImmediateContext.UnmapSubresource(texture, 0);
+                                        texture.Dispose();
+                                    }
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        catch (SharpDXException e)
+                        {
+                            if (e.ResultCode.Code != SharpDX.DXGI.ResultCode.WaitTimeout.Result.Code)
+                            {
+                                throw;
+                            }
+                        }
+                    }
+
+                    callbackTask.Wait();
+
+                    while (finishedWorkItemQueue.TryDequeue(out var finishedWorkItem))
+                    {
+                        foreach (var (texture, mapSource) in finishedWorkItem.resources)
+                        {
+                            device.ImmediateContext.UnmapSubresource(texture, 0);
+                            texture.Dispose();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                if (output1 != null)
+                {
+                    output1.Dispose();
+                }
+
+                if (output != null)
+                {
+                    output.Dispose();
+                }
+
+                if (device != null)
+                {
+                    device.Dispose();
+                }
+
+                if (adapter != null)
+                {
+                    adapter.Dispose();
+                }
+
+                if (factory != null)
+                {
+                    factory.Dispose();
+                }
+            }
+        }
+    }
+}
